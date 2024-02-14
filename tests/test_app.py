0cbda389e8e7e3f2e4db418d4bca4be481e4130b
Boz_Dag <BozDag@umma.tu>
2024-02-14 20:02:37 +0300

    create test_div

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,16 @@
+import pytest
+from application.app import Calculator
 
 
+@pytest.mark.parametrize(
+    "x, y, res",
+    [
+        (1, 2, 0.5),
+        (5, -1, -5),
+        (5, -1, -5.0),
+        (10, 2, 5),
+        (10, 2, 5.0),
+    ]
+)
+def test_div(x, y, res):
+    assert Calculator().div(x, y) == res
