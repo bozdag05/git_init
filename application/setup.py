5a91e96e4fabbc8a1dccbd8630fb21e8f425006f
Boz_Dag <BozDag@umma.tu>
2024-02-16 16:48:45 +0300

    create mypackage

---
+++ application/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mypackage',
+    version='0.1',
+    description='Моя библиотека',
+    url='#',
+    author='auth',
+    author_email='muhammadosmanov02@gmail.com',
+    license='MIT',
+    packages=['mypackage'],
+    zip_safe=False
+)
